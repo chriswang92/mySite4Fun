00651ae75fd36baa9da62b7aca71c631b90b0a4d
bvschriswang <chriswang.bvs@gmail.com>
2019-10-10 20:10:21 -0400

    update

--- src/main/java/code4fun/chriswang/springreactblog/model/User.java
+++ src/main/java/code4fun/chriswang/springreactblog/model/User.java
@@ -1,7 +1,13 @@
 package code4fun.chriswang.springreactblog.model;
 
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+
+//@Entity
 public class User {
 
+//    private @Id @GeneratedValue int id;
     private int id;
     private String username;
     private String password;
